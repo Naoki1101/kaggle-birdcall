9e0a0ff3fc458bc7d1f7b7ac13fcb5d473ecafb7
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-08-30 18:20:59 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -9,13 +9,14 @@ cd ../src
 
 # python train.py -m 'resnet34_001' -c 'epoch=50'
 # python train.py -m 'resnet34_002' -c 'BCEWithLogitsLoss, epoch=30'
-python train.py -m 'resnet34_003' -c 'BCEWithLogitsLoss, epoch=50'
+# python train.py -m 'resnet34_003' -c 'BCEWithLogitsLoss, epoch=50'
 
 # python train.py -m 'resnet50_001' -c 'epoch=50'
 # python train.py -m 'resnet50_002' -c 'epoch=50'
 # python train.py -m 'resnet50_003' -c 'mod head'
 
 # python train.py -m 'efficientnetb3_001' -c 'test'
+python train.py -m 'efficientnetb3_002' -c 'epoch=30'
 
 
 cd ../
