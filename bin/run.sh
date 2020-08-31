5857b2785af3b7ccde809e14916abd6a611d0aa8
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-08-31 22:36:43 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -16,7 +16,9 @@ cd ../src
 # python train.py -m 'resnet50_001' -c 'epoch=50'
 # python train.py -m 'resnet50_002' -c 'epoch=50'
 # python train.py -m 'resnet50_003' -c 'mod head'
-python train.py -m 'resnet50_004' -c 'epoch=30'
+# python train.py -m 'resnet50_004' -c 'epoch=30'
+
+python train.py -m 'senet50_001' -c 'epoch=30'
 
 # python train.py -m 'efficientnetb3_001' -c 'test'
 # python train.py -m 'efficientnetb3_002' -c 'epoch=30'
