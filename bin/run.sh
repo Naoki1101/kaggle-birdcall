25a4f2a012bb494213717d6339dd2e38411acbd1
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-09-06 08:48:20 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -31,7 +31,7 @@ cd ../src
 # python train.py -m 'se_resnext50_32x4d_007' -c 'epoch=30, mod noise'
 # python train.py -m 'se_resnext50_32x4d_008' -c 'epoch=30, mixup, noise'
 # python train.py -m 'se_resnext50_32x4d_009' -c 'epoch=30, mixup, noise, pin_memory=True'
-python train.py -m 'se_resnext50_32x4d_010' -c 'epoch=30, mixup, noise, freesound_noise, shift'
+# python train.py -m 'se_resnext50_32x4d_010' -c 'epoch=30, mixup, noise, freesound_noise, shift'
 python train.py -m 'se_resnext50_32x4d_011' -c 'epoch=30, mixup, shift'
 python train.py -m 'se_resnext50_32x4d_012' -c 'epoch=50, mixup, noise, freesound_noise, shift'
 
