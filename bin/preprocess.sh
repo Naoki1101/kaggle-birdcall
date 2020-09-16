291035a1fadd824ea6ee6510f5e570c0130801da
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-09-17 00:10:09 +0900

    update

--- bin/preprocess.sh
+++ bin/preprocess.sh
@@ -1,12 +1,12 @@
 cd ../src
 
-# python preprocess/01_resample.py
+python preprocess/01_resample.py
 # python preprocess/02_melspectrogram.py
-# python preprocess/03_drop_idx.py
-# python preprocess/04_extract_noise_audio.py
-# python preprocess/05_create_exsample_test.py
-# python preprocess/06_extract_freesound_water_noise.py
-# python preprocess/07_extract_freesound_bus_noise.py
-# python preprocess/08_extract_freesound_walk_noise.py
-# python preprocess/09_extract_freesound_rain_noise.py
-# python preprocess/10_extract_freesound_motorcycle_noise.py
+python preprocess/03_drop_idx.py
+python preprocess/04_extract_noise_audio.py
+python preprocess/05_create_exsample_test.py
+python preprocess/06_extract_freesound_water_noise.py
+python preprocess/07_extract_freesound_bus_noise.py
+python preprocess/08_extract_freesound_walk_noise.py
+python preprocess/09_extract_freesound_rain_noise.py
+python preprocess/10_extract_freesound_motorcycle_noise.py
